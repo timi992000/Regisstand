974e87792f3d7a1bbd60ce8de84dd2dec3a68c5f
timi992000 <77628993+timi992000@users.noreply.github.com>
2023-07-12 16:21:30 +0200

    choose prefix for auto rename all types with prefix

--- Regisstand/Commands/RenameFileTypesWithPrefixCommand.cs
+++ Regisstand/Commands/RenameFileTypesWithPrefixCommand.cs
@@ -2,101 +2,115 @@
 using Microsoft.VisualStudio.Shell;
 using Regisstand.Core;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Threading.Tasks;
 
 namespace Regisstand.Commands
 {
-  internal sealed class RenameFileTypesWithPrefixCommand
-  {
-    public readonly Package Package;
-    public const int CommandId = 258;
-    public static readonly Guid CommandSet = new Guid("e73b1379-6d9d-4057-adf1-aca51e35f7c4");
-    public RenameFileTypesWithPrefixCommand(Package package, IMenuCommandService commandService)
+    internal sealed class RenameFileTypesWithPrefixCommand
     {
-      if (package == null)
-      {
-        throw new ArgumentNullException("Package");
-      }
-
-      Package = package;
-
-      if (commandService != null)
-      {
-        var menuCommandID = new CommandID(CommandSet, CommandId);
-        var menuItem = new MenuCommand(DoAction, menuCommandID);
-        commandService.AddCommand(menuItem);
-      }
-    }
-
-
-    public static RenameFileTypesWithPrefixCommand Instance
-    {
-      get;
-      private set;
-    }
-
-    public static async Task InitializeAsync(AsyncPackage package)
-    {
-      var svcProvider = await package.GetServiceAsync(typeof(IMenuCommandService));
-      var commandService = svcProvider as OleMenuCommandService;
-      PackageContext.Instance.Package = package as RegisstandPackage;
-      Instance = new RenameFileTypesWithPrefixCommand(package, commandService);
-    }
-
-
-    private void DoAction(object sender, EventArgs e)
-    {
-      __RenameTypes();
-    }
-
-    private void __RenameTypes()
-    {
-      if (PackageContext.Instance == null || PackageContext.Instance.CurrentDocument == null)
-        return;
-      __RenameTypesInCurrentDocument();
-    }
-
-
-    private void __RenameTypesInCurrentDocument()
-    {
-      ThreadHelper.ThrowIfNotOnUIThread();
-      __IterateThroughClasses();
-    }
-
-    private void __IterateThroughClasses()
-    {
-      var textDocument = PackageContext.Instance.DTE.ActiveDocument;
-      var codeModel = textDocument.ProjectItem.FileCodeModel;
-      foreach (EnvDTE.CodeElement codeElement in codeModel.CodeElements)
-      {
-        if (codeElement.Kind == EnvDTE.vsCMElement.vsCMElementNamespace)
+        public readonly Package Package;
+        public const int CommandId = 258;
+        public static readonly Guid CommandSet = new Guid("e73b1379-6d9d-4057-adf1-aca51e35f7c4");
+        public RenameFileTypesWithPrefixCommand(Package package, IMenuCommandService commandService)
         {
-          EnvDTE.CodeNamespace codeNamespace = (EnvDTE.CodeNamespace)codeElement;
-          foreach (EnvDTE.CodeElement nestedCodeElement in codeNamespace.Members)
-          {
-            if (nestedCodeElement.Kind == EnvDTE.vsCMElement.vsCMElementClass)
+            if (package == null)
             {
-              EnvDTE.CodeClass codeClass = (EnvDTE.CodeClass)nestedCodeElement;
-              var prefix = "YOURPREFIX";
-              if (codeClass.Name.StartsWith(prefix))
-                continue;
-              else
-              {
-                codeClass.Name = prefix + codeClass.Name;
-              }
+                throw new ArgumentNullException("Package");
             }
-          }
+
+            Package = package;
+
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(DoAction, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
         }
-      }
+
+
+        public static RenameFileTypesWithPrefixCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            var svcProvider = await package.GetServiceAsync(typeof(IMenuCommandService));
+            var commandService = svcProvider as OleMenuCommandService;
+            PackageContext.Instance.Package = package as RegisstandPackage;
+            Instance = new RenameFileTypesWithPrefixCommand(package, commandService);
+        }
+
+
+        private void DoAction(object sender, EventArgs e)
+        {
+            __RenameTypes();
+        }
+
+        private void __RenameTypes()
+        {
+            if (PackageContext.Instance == null || PackageContext.Instance.CurrentDocument == null)
+                return;
+            __RenameTypesInCurrentDocument();
+        }
+
+
+        private void __RenameTypesInCurrentDocument()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            __IterateThroughClasses();
+        }
+
+        private void __IterateThroughClasses()
+        {
+            var prefix = __GetPrefix();
+            var textDocument = PackageContext.Instance.DTE.ActiveDocument;
+            var codeModel = textDocument.ProjectItem.FileCodeModel;
+            foreach (EnvDTE.CodeElement codeElement in codeModel.CodeElements)
+            {
+                if (codeElement.Kind == EnvDTE.vsCMElement.vsCMElementNamespace)
+                {
+                    EnvDTE.CodeNamespace codeNamespace = (EnvDTE.CodeNamespace)codeElement;
+                    foreach (EnvDTE.CodeElement nestedCodeElement in codeNamespace.Members)
+                    {
+                        if (nestedCodeElement.Kind == EnvDTE.vsCMElement.vsCMElementClass)
+                        {
+                            EnvDTE.CodeClass codeClass = (EnvDTE.CodeClass)nestedCodeElement;
+                            if (codeClass.Name.StartsWith(prefix))
+                                continue;
+                            else
+                            {
+                                codeClass.Name = prefix + codeClass.Name;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private string __GetPrefix()
+        {
+            using (var InputBox = new InputBox("Please type your prefix", new Dictionary<int, string> { { 1, "Prefix" } }, new List<int> { 1 }))
+            {
+                if (InputBox.OpenInputDialog())
+                {
+                    InputBox.OutputValues.TryGetValue(1, out string prefix);
+                    return prefix;
+                }
+                return "";
+            }
+        }
+
+        public void __RenameClass(ProjectItem projectItem, string newClassName)
+        {
+            CodeClass codeClass = projectItem.FileCodeModel.CodeElements.Item(1) as CodeClass;
+            codeClass.Name = newClassName;
+        }
+
+
     }
-
-    public void __RenameClass(ProjectItem projectItem, string newClassName)
-    {
-      CodeClass codeClass = projectItem.FileCodeModel.CodeElements.Item(1) as CodeClass;
-      codeClass.Name = newClassName;
-    }
-
-
-  }
 }
---
+++ Regisstand/Core/InputBox.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Regisstand.Core
+{
+    public class InputBox : IDisposable
+    {
+        private Window m_Window;
+        private readonly Dictionary<int, TextBox> m_TextBoxes;
+        private readonly List<int> m_RequiredFieldIds;
+        private bool m_ClickedOk;
+        private Button m_CancelButton;
+        private Button m_OkButton;
+        #region [Ctor]
+        public InputBox(string Title, Dictionary<int, string> RequestedValueMessages, List<int> RequiredFieldIds)
+        {
+            m_RequiredFieldIds = RequiredFieldIds ?? new List<int>();
+            m_TextBoxes = new Dictionary<int, TextBox>();
+            m_ClickedOk = false;
+            OutputValues = new Dictionary<int, string>();
+            __CreateWindow(Title, RequestedValueMessages);
+        }
+        #endregion
+
+        #region - properties -
+        #region - public properties -
+        public Dictionary<int, string> OutputValues { get; set; }
+        #endregion
+        #endregion
+
+        #region - public methods -
+        #region [OpenInputDialog]
+        public bool OpenInputDialog()
+        {
+            if (m_TextBoxes != null && m_TextBoxes.Any(t => t.Value != null))
+            {
+                var tb = m_TextBoxes.FirstOrDefault().Value;
+                Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Input,
+                  new Action(() =>
+                  {
+                      tb.Focus();
+                      Keyboard.Focus(tb);
+                  }));
+            }
+            m_Window.ShowDialog();
+            return m_ClickedOk;
+        }
+        #endregion
+
+        #region [Dispose]
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (Disposing)
+            {
+                m_CancelButton.Click -= __ButtonCancel_Clicked;
+                m_OkButton.Click -= __ButtonOk_Clicked;
+
+                m_TextBoxes.ToList().ForEach(tb => tb.Value.TextChanged -= __TextBox_TextChanged);
+                m_Window = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(Disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+        #endregion
+
+        #region - private methods -
+
+        #region [__CreateWindow]
+        private void __CreateWindow(string Title, Dictionary<int, string> RequestedValueMessages)
+        {
+            m_Window = new Window()
+            {
+                Width = 500,
+                Height = 80 + RequestedValueMessages.Count * 25,
+                MinWidth = 500,
+                MinHeight = 80 + RequestedValueMessages.Count * 25,
+                Title = Title,
+            };
+            var DlgContentControl = new ContentControl();
+
+            var MainGrid = __CreateMainGrid(RequestedValueMessages);
+
+            DlgContentControl.Content = MainGrid;
+            m_Window.Content = DlgContentControl;
+        }
+        #endregion
+
+        #region [__CreateMainGrid]
+        private Grid __CreateMainGrid(Dictionary<int, string> RequestedValueMessages)
+        {
+            var MainGrid = new Grid();
+            MainGrid.Margin = new Thickness(5);
+            var ButtonStackPanel = new StackPanel()
+            {
+                Orientation = System.Windows.Controls.Orientation.Horizontal,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Bottom,
+                Margin = new Thickness(0, 5, 0, 0),
+            };
+            var OkText = "Ok";
+            var CancelText = "Abbrechen";
+            m_OkButton = new Button { Height = 22, Width = 80, Content = OkText, Margin = new Thickness(0, 0, 5, 0) };
+            m_OkButton.Click += __ButtonOk_Clicked;
+            ButtonStackPanel.Children.Add(m_OkButton);
+            m_CancelButton = new Button { Height = 22, Width = 80, Content = CancelText };
+            m_CancelButton.Click += __ButtonCancel_Clicked;
+            ButtonStackPanel.Children.Add(m_CancelButton);
+            Grid.SetRow(ButtonStackPanel, 1);
+            if (m_RequiredFieldIds.Any())
+                m_OkButton.IsEnabled = false;
+
+            var InputGrid = new Grid();
+            InputGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            InputGrid.ColumnDefinitions.Add(new ColumnDefinition { MaxWidth = 400 });
+
+            int RowCounter = 0;
+            foreach (var RequestedMessage in RequestedValueMessages)
+            {
+                InputGrid.RowDefinitions.Add(new RowDefinition());
+                var DlgLabel = new Label
+                {
+                    Content = RequestedMessage.Value,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Margin = new Thickness(0, -2, 5, 0)
+                };
+                InputGrid.Children.Add(DlgLabel);
+                Grid.SetRow(DlgLabel, RowCounter);
+                Grid.SetColumn(DlgLabel, 0);
+
+                var DlgTextBox = new TextBox
+                {
+                    VerticalAlignment = VerticalAlignment.Top
+                };
+                DlgTextBox.TextChanged += __TextBox_TextChanged;
+                m_TextBoxes.Add(RequestedMessage.Key, DlgTextBox);
+                InputGrid.Children.Add(DlgTextBox);
+                Grid.SetRow(DlgTextBox, RowCounter);
+                Grid.SetColumn(DlgTextBox, 1);
+                RowCounter++;
+            }
+            MainGrid.Children.Add(InputGrid);
+            MainGrid.Children.Add(ButtonStackPanel);
+            Grid.SetRow(InputGrid, 0);
+            return MainGrid;
+        }
+        #endregion
+
+        #region [__ButtonOk_Clicked]
+        private void __ButtonOk_Clicked(object sender, EventArgs e)
+        {
+            m_ClickedOk = true;
+            m_TextBoxes.ToList().ForEach(tb => OutputValues.Add(tb.Key, tb.Value.Text));
+            m_Window.Close();
+        }
+        #endregion
+
+        #region [__ButtonCancel_Clicked]
+        private void __ButtonCancel_Clicked(object sender, EventArgs e)
+        {
+            m_ClickedOk = false;
+            m_Window.Close();
+        }
+        #endregion
+
+        #region [__TextBox_TextChanged]
+        private void __TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var RequiredFields = m_TextBoxes.Where(x => m_RequiredFieldIds.Contains(x.Key));
+            if (RequiredFields.Any())
+                m_OkButton.IsEnabled = RequiredFields.All(y => !string.IsNullOrEmpty(y.Value.Text));
+        }
+        #endregion
+
+        #endregion
+    }
+}
